d59df1dc1f78727d03ab451ec891182c15f6456f
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-05-12 11:31:50 +0530

    nav bar created for login users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EventsComponent } from './events/events.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, RegisterComponent, HeaderComponent, LoginComponent, HomeComponent, DashboardComponent, EventsComponent],
+  declarations: [AppComponent, RegisterComponent, HeaderComponent, LoginComponent, HomeComponent, DashboardComponent, EventsComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
